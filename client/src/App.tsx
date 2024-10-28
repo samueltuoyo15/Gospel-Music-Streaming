87e7f240f7a4b5f4df76e3252829ef756391079e
samueltuoyo15 <samueltuoyo9082@gmail.com>
2024-10-28 12:48:22 +0100

    updated readme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Home />} />
+     <Route path="/" element={<Home />}/>
       </Routes>
     </Router>
   )
--- client/src/Pages/ControlPanel.tsx
+++
@@ -1,9 +0,0 @@
-function ControlPanel(){
-  return(
-      <section className="select-none text-white mt-5 p-4">
-        <img src="/album.jpeg" className="w-full rounded" />
-      </section>
-    )
-}
-
-export default ControlPanel
